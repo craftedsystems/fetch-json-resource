0f631bef7fe03522c72f901b30967a682f588a5a
Mario Pizzinini <hello@crafted.systems>
2020-01-08 13:48:38 +0100

    cleanup

--- fetch-json-resource.js
+++ fetch-json-resource.js
@@ -93,7 +93,7 @@ async function toJSON(response) {
  * @param {string} url
  * @param {object} options
  */
-export default async function fetchJson(url, options) {
+export default async function fetchJsonResource(url, options) {
   const fetchOptions = hasAbortController()
     ? addAbortController(options)
     : options;
