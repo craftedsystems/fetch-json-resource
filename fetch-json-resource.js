f382eb5566d27179e151a7b090acba4e817deba4
Mario Pizzinini <hello@crafted.systems>
2020-07-13 14:00:47 +0200

    remove error codes

--- fetch-json-resource.js
+++ fetch-json-resource.js
@@ -1,32 +1,47 @@
-// @ts-check
+/**
+ * Check if error is an Abort Controller timeout error
+ * @param {object} error
+ * @param {number?} error.code
+ * @param {string?} error.name
+ * @returns {boolean}
+ */
+function isAbortControllerTimeoutEvent(error) {
+  return (
+    error &&
+    error.code === 20 &&
+    error.name === 'AbortError'
+  );
+}
 
 /**
  * Map network error
  * @param {object} error
+ * @param {number?} error.code
+ * @param {string?} error.id
+ * @param {string?} error.message
+ * @param {string?} error.name
+ * @param {string?} error.type
  */
 function mapNetworkError(error) {
-  if (error && error.code === 20 && error.name === 'AbortError') {
-    // Abort controller event
-    error = {
-      code: -1,
+  if (isAbortControllerTimeoutEvent(error)) {
+    return Promise.reject({
       id: 'network-timeout',
       message: 'Network timeout',
       type: 'app:network-timeout',
-    };
-  } else {
-    error = {
-      code: error.code || -1,
-      id: error.id || 'network-error',
-      message: error.message || 'unknown error',
-      type: error.type || 'app:unknown-error',
-    };
+    });
   }
-  return Promise.reject(error);
+  return Promise.reject({
+    id: error.id || 'network-error',
+    message: error.message || 'unknown network error',
+    type: error.type || 'app:unknown-network-error',
+  });
 }
 
 /**
  * Check for AbortController support
- * base on https://github.com/mo/abortcontroller-polyfill/blob/f20480d50d642d65790ee1af80e54a71506bcd46/src/abortcontroller-polyfill.js
+ * base on
+ * https://github.com/mo/abortcontroller-polyfill/blob/f20480d50d642d65790ee1af80e54a71506bcd46/src/abortcontroller-polyfill.js
+ * https://github.com/mo/abortcontroller-polyfill/blob/f20480d50d642d65790ee1af80e54a71506bcd46/src/utils.js
  */
 function hasAbortController() {
   const self = typeof window !== 'undefined' && typeof window.self !== 'undefined'
@@ -54,7 +69,6 @@ function hasAbortController() {
  */
 function addAbortController(options, timeout) {
   const controller = new AbortController();
-  // Abort request after 10 sec
   setTimeout(() => controller.abort(), timeout);
   return {
     ...options,
@@ -78,8 +92,8 @@ function getLastErrorTypeSegment(type) {
 async function toJSON(response) {
   try {
     const json = await response.json();
-    if (json.code) {
-      json.id = json.id || getLastErrorTypeSegment(json.type);
+    if (json.type) {
+      json.id = getLastErrorTypeSegment(json.type);
       return Promise.reject(json);
     }
     return json;
@@ -91,8 +105,8 @@ async function toJSON(response) {
 /**
  * Fetch resource
  * @param {string} url
- * @param {object} options
- * @param {number} timeout in milliseconds (default 10000)
+ * @param {object=} options
+ * @param {number=} timeout in milliseconds (default 10000)
  */
 async function fetchJsonResource(url, options, timeout = 10000) {
   const fetchOptions = hasAbortController()
---
+++ types.js
@@ -0,0 +1,7 @@
+/**
+ * Declare `ApiError` type
+ * @typedef {object} ApiError
+ * @property {string} id
+ * @property {string} message
+ * @property {string} type
+ */
